ecb425e689333b74e5a304a78ea5c036f4b7ded9
Adam Wickström <adwi02001@hotmail.com>
2019-10-10 17:45:12 +0200

    gjorde en alternativ lösning till filip

--- filip/filip/Program.cs
+++ filip/filip/Program.cs
@@ -12,16 +12,12 @@ namespace Filip
             int tal2 = int.Parse(Console.ReadLine());
             int reverse = 0;
             int reverse2 = 0;
-            while (0 < tal1 && tal1 < 1000)
+            while (0 < tal1 && tal2 < 1000)
             {
                 int remainder = tal1 % 10;
                 reverse = (reverse * 10) + remainder;
                 tal1 = tal1 / 10;
 
-            }
-
-            while (0 < tal2 && tal2 < 1000)
-            {
                 int remainder2 = tal2 % 10;
                 reverse2 = (reverse2 * 10) + remainder2;
                 tal2 = tal2 / 10;
---
+++ filip2.0/filip2.0/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace filip2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, n2, reverse=0, reverse2 = 0, rem;
+            Console.WriteLine("skriv in ett tresiffrigt heltal");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("skriv in ett annat tresiffrigt heltal");
+            n2 = int.Parse(Console.ReadLine());
+            while(n!=0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n /=10;
+
+                rem = n2 % 10;
+                reverse2 = reverse2 * 10 + rem;
+                n2 /= 10;
+            }
+            
+            if(reverse<reverse2)
+            {
+                Console.WriteLine(reverse2);
+            }
+
+            else
+            {
+                Console.WriteLine(reverse);
+            }
+            
+
+        }
+
+    }
+}
